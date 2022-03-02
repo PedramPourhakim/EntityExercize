6f49b99bc2b578112d705b243664e29342dd38f1
PedramPourhakim <shemuel1226@gmail.com>
2022-03-02 16:02:58 +0330

    ProductModelCreated

---
+++ Solution1/EntityExercize/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityExercize.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Product(string name,double unitprice)
+        {
+            Name = name;
+            UnitPrice = unitprice;
+            CreationDate = DateTime.Now;
+        }
+    }
+}
